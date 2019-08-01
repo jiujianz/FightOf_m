9368376e7c9d814b9ffa04602af28d5846178425
jiujianz <cocobskg@gmail.com>
2019-08-01 21:22:00 +0900

    display-all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user, only: [:destoroy]
 
   def index
-    @user = User.all
+    @users = User.all
   end
 
   def show
