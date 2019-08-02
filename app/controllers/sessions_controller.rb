11a9b39daf9ff7335afa9d2eb5808446458e9652
jiujianz <cocobskg@gmail.com>
2019-08-02 11:11:10 +0900

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,12 @@ class SessionsController < ApplicationController
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_to root_path
-    else
+      else
       message  = "Account not activated. "
       message += "Check your email for the activation link."
       flash[:warning] = message
       render 'new'
+      end
     end
   end
   
