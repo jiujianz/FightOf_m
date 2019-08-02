384b10f0e1e66e4fd0d136d90a3165d7c5cbc494
jiujianz <cocobskg@gmail.com>
2019-08-02 15:48:54 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,10 +42,10 @@ class User < ApplicationRecord
 
   # アカウントを有効にする
   def activate
-    update_columns(activated: FILL_IN, activated_at: FILL_IN)
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
   end
 
-  # 有効化用のメールを送信する
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
